19adf230d3b79d54bd4bae15804646c26bc884f2
Ethan Christensen <ethan.christensen@sdl.usu.edu>
2025-05-07 17:23:02 -0600

    Implement check - skip products without 11 factor

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 pub type Int = u32;
 
+#[allow(clippy::useless_conversion)]
+#[inline]
 pub fn is_palindrome(n: Int) -> bool {
-  let mut num: u64 = n.into();
-  let mut rev = 0u64;
+  let mut num = n;
+  let mut rev = 0;
   while num > 0 {
     rev = rev * 10 + num % 10;
     num /= 10;
   }
-  rev == n.into()
+  rev == n
 }
 
 const A: Int = 900;
@@ -15,21 +17,55 @@ const D_ARR: [Int; 2] = [900, 800];
 const B_ARR: [Int; 10] = [90, 80, 70, 60, 50, 40, 30, 20, 10, 0];
 const E_ARR: [Int; 10] = B_ARR;
 const CF_ARR: [(Int, Int); 4] = [(9, 1), (7, 7), (3, 3), (1, 9)];
+#[inline]
 /// Assume digits = 3
 pub fn find_palindrome_product () -> Option<Int> {
   for d in D_ARR {
     for b in B_ARR {
       for e in E_ARR {
         for (c, f) in CF_ARR {
-          let num = (A + b + c) * (d + e + f);
-          if is_palindrome(num) {
-            return Some(num)
+          let num_1 = A + b + c;
+          let num_2 = d + e + f;
+          
+          if num_1 % 11 != 0 && num_2 % 11 != 0 {
+            continue
+          }
+
+          let prod = num_1 * num_2;
+          if is_palindrome(prod) {
+            return Some(prod)
           }
         }
       }
     }
   }
   None
+}
+
+fn list_checked_numbers () -> Int {
+  let mut count = 0;
+  for d in D_ARR {
+    for b in B_ARR {
+      for e in E_ARR {
+        for (c, f) in CF_ARR {
+          let num_1 = A + b + c;
+          let num_2 = d + e + f;
+          
+          if num_1 % 11 != 0 && num_2 % 11 != 0 {
+            continue
+          }
+
+          let prod = num_1 * num_2;
+          println!("checked {num_1} () * {num_2} = {prod}");
+          count += 1;
+          if is_palindrome(prod) {
+            return count;
+          }
+        }
+      }
+    }
+  }
+  count
 }
 
 #[cfg(test)]
@@ -53,5 +89,11 @@ mod tests {
       (123321, true)
     );
     inputs.into_iter().for_each(|input| assert_eq!(is_palindrome(input.0), input.1));
+  }
+
+  #[test]
+  fn list_checked_numbers_dbg () {
+    let num_checked = list_checked_numbers();
+    println!("checked only {num_checked} products");
   }
 }
